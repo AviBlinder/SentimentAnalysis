2e5225de16f8c796ecf6f5d8a21df1e7d4b2e9be
User 1 <user1@ip-172-31-5-188.ec2.internal>
2016-10-09 18:09:54 +0000

    post_process additions

--- R/post_process_analysis.r
+++ R/post_process_analysis.r
@@ -1,5 +1,5 @@
 head(out_file)
-
+head(r1)
 plot(out_file$sentiment_grade,out_file$review_stars,xlab = "Classification",ylab = "User's rating")
 round(prop.table(table(out_file$sentiment_grade)),2)
 round(prop.table(table(out_file$s)),2)
@@ -9,6 +9,7 @@ round(prop.table(table(out_file$s)),2)
 hist(out_file$review_stars)
 
 star1 <- subset(out_file,out_file$review_stars == 1)
+table(star1$sentiment_grade)
 prop.table(table(star1$sentiment_grade))
 
 star2 <- subset(out_file,out_file$review_stars == 2)
@@ -22,3 +23,9 @@ prop.table(table(star4$sentiment_grade))
 
 star5 <- subset(out_file,out_file$review_stars == 5)
 prop.table(table(star5$sentiment_grade))
+
+
+star1_pos <- r1[out_file$review_stars == 1 & out_file$sentiment_grade == "Pos",]
+star1_pos[1,]
+star1_pos_outfile <- out_file[out_file$review_stars == 1 & out_file$sentiment_grade == "Pos",]
+star1_pos_outfile[1,]
