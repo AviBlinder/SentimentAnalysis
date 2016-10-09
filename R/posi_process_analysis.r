4f46f416c3c538da14c135e90ff049237dd41932
User 1 <user1@ip-172-31-6-194.ec2.internal>
2016-10-09 10:01:47 +0000

    minor changes

--- R/posi_process_analysis.r
+++ R/posi_process_analysis.r
@@ -1,23 +1,24 @@
-r1 <- out_file
-plot(r1$sentiment_grade,r1$review_stars,xlab = "Classification",ylab = "User's rating")
-round(prop.table(table(r1$sentiment_grade)),2)
-round(prop.table(table(r1$s)),2)
+head(out_file)
 
-#plot(table(r1$sentiment_grade))
+plot(out_file$sentiment_grade,out_file$review_stars,xlab = "Classification",ylab = "User's rating")
+round(prop.table(table(out_file$sentiment_grade)),2)
+round(prop.table(table(out_file$s)),2)
 
-hist(r1$review_stars)
+#plot(table(out_file$sentiment_grade))
 
-star1 <- subset(r1,r1$review_stars == 1)
-prop.table(table(star1$sentiment_grade))
+hist(out_file$review_stars)
 
-star2 <- subset(r1,r1$review_stars == 2)
+staout_file <- subset(out_file,out_file$review_stars == 1)
+prop.table(table(staout_file$sentiment_grade))
+
+star2 <- subset(out_file,out_file$review_stars == 2)
 prop.table(table(star2$sentiment_grade))
 
-star3 <- subset(r1,r1$review_stars == 3)
+star3 <- subset(out_file,out_file$review_stars == 3)
 prop.table(table(star3$sentiment_grade))
 
-star4 <- subset(r1,r1$review_stars == 4)
+star4 <- subset(out_file,out_file$review_stars == 4)
 prop.table(table(star4$sentiment_grade))
 
-star5 <- subset(r1,r1$review_stars == 5)
+star5 <- subset(out_file,out_file$review_stars == 5)
 prop.table(table(star5$sentiment_grade))
