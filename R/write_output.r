b609c887b4fc35a8333ea2062a60e412b430fb97
Avi Blinder <aviblinder@gmail.com>
2016-10-11 21:43:23 +0300

    minor changes 21:43

--- R/write_output.r
+++ R/write_output.r
@@ -5,6 +5,6 @@ if(sentiment == 0) sentiment_grade <- "Neu"
 
 
 record <- cbind(r1$stars[i], sentiment, sentiment_grade,r1$business_id[i],
-                r1$user_id[i],r1$Maincity[i],CorpusDescriptionUpper[[1]]$content)
+                r1$user_id[i],r1$Maincity[i],CorpusDescription[[1]]$content)
 
 write.table(record,file=file_name,append = TRUE,sep=",",row.names = FALSE,col.names = FALSE)
